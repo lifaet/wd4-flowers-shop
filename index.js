03c8df3e981d2be6c941c274836409770cbdbf73
Lifaet Hossain Zim <lifaethossain@gmail.com>
2023-07-11 02:50:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 
 $("#home").click(function () {
     $('html,body').animate({
-        scrollTop: $(".font-container").offset().top - 80
+        scrollTop: $(".font-container").offset().top - 45
     },
         'fast');
 });
 $("#feature").click(function () {
     $('html,body').animate({
-        scrollTop: $(".features-container").offset().top - 80
+        scrollTop: $(".features-container").offset().top - 45
     },
         'fast');
 });
 $("#featurex").click(function () {
     $('html,body').animate({
-        scrollTop: $(".features-container").offset().top - 80
+        scrollTop: $(".features-container").offset().top - 45
     },
         'fast');
 });
 $("#hot").click(function () {
     $('html,body').animate({
-        scrollTop: $(".hot-container").offset().top - 80
+        scrollTop: $(".hot-container").offset().top - 45
     },
         'fast');
 });
 $("#insta").click(function () {
 
     $('html,body').animate({
-        scrollTop: $(".insta-container").offset().top - 80
+        scrollTop: $(".insta-container").offset().top - 45
     },
         'fast');
 });
