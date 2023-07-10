ebbcbf89ba669604d1a76e2e888046df88b57f3e
Lifaet Hossain Zim <lifaethossain@gmail.com>
2023-07-11 02:42:44 +0600

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+
+$("#home").click(function () {
+    $('html,body').animate({
+        scrollTop: $(".font-container").offset().top - 80
+    },
+        'fast');
+});
+$("#feature").click(function () {
+    $('html,body').animate({
+        scrollTop: $(".features-container").offset().top - 80
+    },
+        'fast');
+});
+$("#featurex").click(function () {
+    $('html,body').animate({
+        scrollTop: $(".features-container").offset().top - 80
+    },
+        'fast');
+});
+$("#hot").click(function () {
+    $('html,body').animate({
+        scrollTop: $(".hot-container").offset().top - 80
+    },
+        'fast');
+});
+$("#insta").click(function () {
+
+    $('html,body').animate({
+        scrollTop: $(".insta-container").offset().top - 80
+    },
+        'fast');
+});
